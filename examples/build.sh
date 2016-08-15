d34ba5429f0fd1d3f1034f92a3d6091e481fb127
Mario Manno <mario.manno@suse.com>
2016-08-15 11:03:19 +0200

    update

--- examples/build.sh
+++ examples/build.sh
@@ -3,6 +3,10 @@ set -e
 sed -i "s|<username>|$OSC_USERNAME|g" /root/.oscrc
 sed -i "s|<password>|$OSC_PASSWORD|g" /root/.oscrc
 
-osc log --csv
+pushd osc-resource/home:mmanno/restic
 
-osc service run
+  osc log --csv
+
+  osc service run
+
+popd
