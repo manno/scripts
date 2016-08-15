cd9a86a05d9c04139b836ece25bc20c53152f66d
Mario Manno <mario.manno@suse.com>
2016-08-15 11:10:54 +0200

    install scm

--- examples/build.sh
+++ examples/build.sh
@@ -3,6 +3,7 @@ set -e
 sed -i "s|<username>|$OSC_USERNAME|g" /root/.oscrc
 sed -i "s|<password>|$OSC_PASSWORD|g" /root/.oscrc
 
+zypper -n in obs-service-tar_scm
 pushd osc-resource/home:mmanno/restic 1> /dev/null
 
   osc log --xml | tee
