fdcf3991d795e2f5e82435a0478595696902cc7b
Mario Manno <mario.manno@suse.com>
2016-08-15 11:08:57 +0200

    dont wait for return

--- examples/build.sh
+++ examples/build.sh
@@ -3,10 +3,10 @@ set -e
 sed -i "s|<username>|$OSC_USERNAME|g" /root/.oscrc
 sed -i "s|<password>|$OSC_PASSWORD|g" /root/.oscrc
 
-pushd osc-resource/home:mmanno/restic
+pushd osc-resource/home:mmanno/restic 1> /dev/null
 
-  osc log --csv
+  osc log --xml | tee
 
   osc service run
 
-popd
+popd 1> /dev/null
