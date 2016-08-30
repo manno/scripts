3cca0126d6bac17a50b3366991af83447d8dbb80
Mario Manno <mario.manno@suse.com>
2016-08-30 16:34:04 +0200

    fix pipeline and add alternative

--- ci/osc-restic/tasks/run-service.sh
+++ ci/osc-restic/tasks/run-service.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+if [ -z "$TOKEN" ]; then
+  echo "Missing token in environment."
+  env
+  exit 1
+fi
 curl -sS -H "Authorization: Token $TOKEN" -X POST 'https://api.opensuse.org/trigger/runservice?project=home:mmanno&package=restic'
